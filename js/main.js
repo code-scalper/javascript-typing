2a8ab6d48bf0065692f78de2f972635b33827348
Lee <tigerbaram@gmail.com>
2020-12-02 17:50:53 +0900

    'first'

--- js/main.js
+++ js/main.js
@@ -1,85 +1,117 @@
-const url = "https://random-word-api.herokuapp.com/word?number=100";
-const wordInput = document.querySelector("#word-input")
-const scoreDisplay = document.querySelector("#score")
-const message = document.querySelector("#message")
-const timeDisplay = document.querySelector("#time")
+
+
+// 사용 변수
+const SETTING_TIME = 10;
+let words = [];
+let time;
+let isPlaying = false;
 let score = 0;
-const gameTime = 5;
-let time = gameTime;
-let isPlaying = true;
 
 
-const words = [];
+const url = "https://random-word-api.herokuapp.com/word?number=100";
+const timeDisplay = document.querySelector('.time')
+const button = document.querySelector('.button')
+const wordDisplay = document.querySelector('.word-display')
+const wordInput = document.querySelector('.word-input')
+const scoreDisplay = document.querySelector('.score')
 
 
-
-// 시작되면
 init();
-
-
-// initial 초기 실행
 function init() {
     getWords();
-    timeDisplay.innerHTML = time;
-    wordInput.addEventListener('input', startMatch)
-    setInterval(countDown, 1000)
-    setInterval(checkStatus, 50)
+    wordInput.addEventListener('input', checkMatch)
+}
 
+
+function checkStatus() {
+    if (!isPlaying && time === 0) {
+        isPlaying = false;
+        buttonChange('start', '게임시작');
+        clearInterval(checkInterval)
+    }
+}
+
+function checkMatch() {
+    if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
+        wordInput.value = "";
+        if (!isPlaying) {
+            runNotification('error')
+            return
+        }
+        score++;
+        scoreDisplay.innerText = score;
+        time = SETTING_TIME;
+        const randomIndex = Math.floor(Math.random() * words.length)
+        wordDisplay.innerText = words[randomIndex];
+        runNotification('success')
+    }
+}
+
+
+
+
+function run() {
+    if (words.length < 1) {
+        return;
+    }
+    wordInput.value = "";
+    wordInput.focus()
+    score = 0;
+    scoreDisplay.innerText = 0;
+    time = SETTING_TIME;
+    isPlaying = true;
+    timeInterval = setInterval(countDown, 1000)
+    checkInterval = setInterval(checkStatus, 50)
+    buttonChange('loading', '게임중')
+}
+
+function countDown() {
+    time > 0 ? time-- : isPlaying = false;
+    timeDisplay.innerText = time;
+    if (!isPlaying) {
+        clearInterval(timeInterval)
+    }
+    console.log('count')
 }
 
 // 단어 가져오기
 function getWords() {
     axios.get(url).then((res) => {
+
         res.data.forEach((word) => {
-            if (word.length < 10) {
+            if (word.length < 7) {
                 words.push(word);
             }
+            buttonChange('start', '게임시작')
         })
     }).catch((err) => {
         console.log(err);
     })
 }
 
-
-// 함수들
-// 게임이 플레이중인지 확인
-function checkStatus() {
-    if (!isPlaying && time === 0) {
-        message.innerHTML = 'Game Over!!!';
-        score = -1;
-    }
+function buttonChange(type, text) {
+    button.innerText = text;
+    type === 'loading' ? button.classList.add('loading') : button.classList.remove('loading')
 }
 
-// 시간을 카운트 다운
-function countDown() {
-    if (time > 0) {
-        time--;
-    } else if (time === 0) {
-        isPlaying = false;
-    }
-    timeDisplay.innerHTML = time;
-}
-// 글자가 맞는지 확인 함수
-function startMatch() {
-    const currentWord = document.querySelector("#current-word");
-    // console.log(currentWord.innerHTML, wordInput.value, currentWord.innerHTML === wordInput.value)
 
-    if (currentWord.innerHTML === wordInput.value) {
-        score++;
-        scoreDisplay.innerHTML = score
-        time = gameTime;
-        wordInput.value = "";
-        message.innerHTML = '정답 입니다!!!'
-        const randomIndex = Math.floor(Math.random() * words.length)
-        console.log(randomIndex);
-        currentWord.innerHTML = words[randomIndex];
-    } else {
-        message.innerHTML = ''
+
+function runNotification(type) {
+    // toastify options
+    const option = {
+        text: `${wordDisplay.innerText}!!`,
+        duration: 3000,
+        newWindow: true,
+        gravity: "top", // `top` or `bottom`
+        position: 'left', // `left`, `center` or `right`
+        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)"
+    }
+    if (type === 'error') {
+        option.text = '우선 게임시작 버튼을 눌러주세요'
+        option.position = 'right'
+        option.backgroundColor = 'red'
     }
 
-    if (score === -1) {
-        scoreDisplay.innerHTML = 0;
-    } else {
-        scoreDisplay.innerHTML = score
-    }
+    Toastify(option).showToast();
 }
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+1. 인풋 입력시 점수 1 ++
+2. word display 단어 넣기, 일단 array(5) 인풋창 초기화
+3. 버튼 클릭시 카운트다운 
+4. 버튼 클릭시 게임중으로 바꾸기 buttonchange function
+5. Toastify Library
